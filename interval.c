9d50a697aebb29096e1093cf880cd46289b2f7fc
Mateus Gomes <35661150+mateusliderprojetos@users.noreply.github.com>
2018-08-26 22:07:25 -0300

    Create interval.c
    Algoritmo para calcular intervalo entre dois números

---
+++ interval.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int x;
+
+	printf("Informe o valor de X:");
+	scanf("%d",& x);
+
+if(x>=0 . x<20 ){
+	printf("Esta entre 0 e 20 \n");
+}
+else {
+	printf("Nao estao entre 0 e 20 \n");
+}
+}
+
